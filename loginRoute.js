10770b7e452f729fe5054ceb300d3f1e0bd7ee1b
Spencer Bohol <spencerbohol3@gmail.com>
2024-07-04 17:53:28 +0800

    removed comments

--- loginRoute.js
+++ loginRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
 const User = require("./models/User");
 const router = express.Router();
 const bcrypt = require("bcrypt");
